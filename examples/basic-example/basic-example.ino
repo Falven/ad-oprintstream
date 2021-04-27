9bf095afa460ba6471cb2703b8b3166e48b55544
Fran Aguilera <fraguile@microsoft.com>
2021-04-27 17:19:16 -0400

    Example description.

--- examples/basic-example/basic-example.ino
+++ examples/basic-example/basic-example.ino
@@ -6,12 +6,24 @@
 
 using namespace falven::ad;
 
-OPrintStream print(USBDevice);
+// Our global printing object.
+extern OPrintStream print(USBDevice);
 
 void setup() {
+  // Your typical Serial initialization.
   print.begin(9600UL);
   while (!print) {
   }
+
+  // Which looks better, more succint, and extendable!?
+  print.print("Testing... ");
+  print.print(1, BIN);
+  print.print(" ");
+  print.print(2, BIN);
+  print.println();
+  print.flush();
+
+  // I think you guessed correctly...
   print << bin << "Testing... " << 1 << " " << 2 << endl;
 }
 
